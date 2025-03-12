5423830ddf0947441e4bf186242b73e8d99e443a
fabianpradod <fabian.prado2004@gmail.com>
2025-03-11 19:42:35 -0600

    Se creo la clase de Pokemon

--- src/main/java/com/hdt6/Pokemon.java
+++ src/main/java/com/hdt6/Pokemon.java
@@ -1,5 +1,124 @@
 package com.hdt6;
+/**
+ * Autor: Fabian Prado Dluzniewski #23427
+ * Curso: Algoritmos y Estructuras de Datos
+ * Profesor: Douglas Barrios
+ * Universidad del Valle de Guatemala
+ *
+ * Descripción: Clase de cada Pokémon con su información básica
+ */
 
-public class Pokemon {
-    
-}
+ import java.util.List;
+
+ public class Pokemon {
+     private String name;
+     private int pokedexNumber;
+     private String type1;
+     private String type2;
+     private String classification;
+     private double height;
+     private double weight;
+     private List<String> abilities;
+     private int generation;
+     private boolean isLegendary;
+ 
+     public Pokemon(String name, int pokedexNumber, String type1, String type2, String classification,
+                    double height, double weight, List<String> abilities, int generation, boolean isLegendary) {
+         this.name = name;
+         this.pokedexNumber = pokedexNumber;
+         this.type1 = type1;
+         this.type2 = type2;
+         this.classification = classification;
+         this.height = height;
+         this.weight = weight;
+         this.abilities = abilities;
+         this.generation = generation;
+         this.isLegendary = isLegendary;
+     }
+ 
+     public String getName() {
+        return name; 
+    }
+    public void setName(String name) {
+        this.name = name; 
+    }
+
+     public int getPokedexNumber() {
+        return pokedexNumber;
+    }
+     public void setPokedexNumber(int pokedexNumber) {
+        this.pokedexNumber = pokedexNumber;
+    }
+
+     public String getType1() {
+        return type1; 
+    }
+     public void setType1(String type1) {
+        this.type1 = type1; 
+    }
+
+     public String getType2() {
+        return type2;
+    }
+     public void setType2(String type2) {
+        this.type2 = type2;
+    }
+
+     public String getClassification() {
+        return classification;
+    }
+     public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+     public double getHeight() {
+        return height;
+    }
+     public void setHeight(double height) {
+        this.height = height;
+    }
+
+     public double getWeight() {
+        return weight;
+    }
+     public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+     public List<String> getAbilities() {
+        return abilities;
+    }
+     public void setAbilities(List<String> abilities) {
+        this.abilities = abilities;
+    }
+
+     public int getGeneration() {
+        return generation;
+    }
+     public void setGeneration(int generation) {
+        this.generation = generation;
+    }
+
+     public boolean isLegendary() {
+        return isLegendary;
+    }
+     public void setLegendary(boolean legendary) {
+        isLegendary = legendary;
+    }
+ 
+     @Override
+     public String toString() {
+         return "Pokemon{" +
+                 "Nombre ='" + name + '\'' +
+                 ", Numero de pokede x=" + pokedexNumber +
+                 ", Type1 ='" + type1 + '\'' +
+                 ", Type2 ='" + type2 + '\'' +
+                 ", Clasificacion ='" + classification + '\'' +
+                 ", Altur a=" + height +
+                 ", Peso =" + weight +
+                 ", Habilidades =" + abilities +
+                 ", Generacion =" + generation +
+                 ", Legendario =" + isLegendary +
+                 '}';
+     }
+ }
